56ef5a770824a45c6587c12d2b7eb96a7b97ae25
cadusaboya <carlosepsaboya@gmail.com>
2024-06-18 16:16:15 -0400

    Improved subtext in Main and added border to the image

--- components/Main.tsx
+++ components/Main.tsx
@@ -6,17 +6,17 @@ const Main = () => {
       <div className="flex-1 mb-8 md:mb-0 md:mr-8 px-20">
         <h1 className="text-4xl md:text-6xl font-extrabold text-purple-900">The future of Manganese is here</h1>
         <p className="mt-2 md:text-x1 max-w-xl text-purple-800 text-justify">
-          MnCoin is a decentralized Manganese Ore Mine powered by blockchain technology. Our mission is to facilitate investments in the ore mining sector and also create a hub for buyers and sellers to trade Manganese safely.
+          MnCoin is a decentralized Manganese Ore Mine powered by blockchain technology. Our mission is to open investments in the ore mining sector to the average investor and also create a hub for buyers and sellers to trade Manganese safely.
         </p>
       </div>
       <div className="flex-1">
         {/* Placeholder for Image or Video */}
         <Image
           src="/mina-placeholder.jpg"
-          alt="About Us Image"
+          alt="Main Image"
           width={500}
           height={300}
-          className="rounded-lg shadow-lg"
+          className="rounded-lg shadow-lg border-2 border-purple-900"
         />
         {/* If you decide to use a video, you can replace the above Image component with the below video tag */}
         {/* <video
