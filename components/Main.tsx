ee934dddbab34735362b102163b4f10c672cca40
cadusaboya <carlosepsaboya@gmail.com>
2024-06-18 16:19:55 -0400

    Changed from here to now in Main title

--- components/Main.tsx
+++ components/Main.tsx
@@ -4,7 +4,7 @@ const Main = () => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between px-8 py-16 bg-gray-100">
       <div className="flex-1 mb-8 md:mb-0 md:mr-8 px-20">
-        <h1 className="text-4xl md:text-6xl font-extrabold text-purple-900">The future of Manganese is here</h1>
+        <h1 className="text-4xl md:text-6xl font-extrabold text-purple-900">The future of Manganese is now</h1>
         <p className="mt-2 md:text-x1 max-w-xl text-purple-800 text-justify">
           MnCoin is a decentralized Manganese Ore Mine powered by blockchain technology. Our mission is to open investments in the ore mining sector to the average investor and also create a hub for buyers and sellers to trade Manganese safely.
         </p>
