4b1be1aef0c90992c2702edbb11f295cbd7feea6
cadusaboya <carlosepsaboya@gmail.com>
2024-06-19 16:56:49 -0400

    Increased image size

--- components/Roadmap.tsx
+++ components/Roadmap.tsx
@@ -37,12 +37,12 @@ const Roadmap = () => {
     <div className="pb-2 pt-5 mx-8 flex flex-col items-center">
       <h1 className="text-4xl md:text-4xl font-extrabold text-purple-900">Roadmap</h1>
 
-      <div className="mt-5">
+      <div>
         <Image
           src="/roadmap.png"
           alt="Roadmap Image"
-          width={1000}
-          height={1000}
+          width={2000}
+          height={2000}
           className="fade-in"
           ref={imgRef}
         />
