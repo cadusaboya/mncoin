92bcbaeb65b2a07b05b1960b594ab3fdc007e0b1
cadusaboya <carlosepsaboya@gmail.com>
2024-06-22 20:15:12 -0400

    Remove intro video

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,15 @@
-"use client"
 import Header from "@/components/Header";
-import { Button } from "@/components/ui/button"
 import Main from "@/components/Main";
 import Goals from "@/components/Goals";
 import Roadmap from "@/components/Roadmap";
 import Founders from "@/components/Founders";
-import React, { useState } from 'react';
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import React from 'react';
+
 
 
 export default function Home() {
-  const [showVideo, setShowVideo] = useState(true); // State to toggle video
-
-  const handleButtonClick = () => {
-    setShowVideo(false); // Hide video and show main content
-  };
-
   return (
     <main>
-      {showVideo && (
-        <div className="video-background">
-          <video autoPlay muted loop playsInline className="video">
-            <source src="/intro.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <Button variant="ghost" className="centered-button" onClick={handleButtonClick}>Enter the Mining World <MdOutlineKeyboardArrowRight className="ml-2 h-6 w-6" />
-          </Button>  
-        </div>
-      )}
-      {!showVideo && (
-        <div className="main-content">
       <header>
         <Header />
       </header>
@@ -49,9 +29,6 @@ export default function Home() {
         <section id="team" className="mt-10">
           <Founders />
         </section>
-        </div>
-      )}
-
       </main>
   );
 }
