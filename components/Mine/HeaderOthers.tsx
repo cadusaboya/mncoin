c34e8dc5752731cd11420092967b429283566cba
cadusaboya <carlosepsaboya@gmail.com>
2025-02-05 20:44:13 -0300

    Fixed logo on other pages

--- components/Mine/HeaderOthers.tsx
+++ components/Mine/HeaderOthers.tsx
@@ -15,7 +15,7 @@ const HeaderOthers = () => {
     <header className="fixed z-50 bg-white flex items-center w-screen h-16 px-4">
       <div className="flex-shrink-0">
         <Image
-          src="/mnlogo.png"
+          src="/mnttext.png"
           alt="MnCoin Logo"
           width={100}
           height={100}
