b3a25dd50f2986a942a1e271311f567b5dc989e3
cadusaboya <carlosepsaboya@gmail.com>
2025-09-13 14:09:22 -0400

    Removed invalid file

--- test.ts
+++
@@ -1,9 +0,0 @@
-const pythClient = new PythHttpClient(connection, pythPublicKey);
-const data = await pythClient.getData();
-
-for (let symbol of data.symbols) {
-  const price = data.productPrice.get(symbol)!;
-  // Sample output:
-  // Crypto.SRM/USD: $8.68725 ±$0.0131 Status: Trading
-  console.log(`${symbol}: $${price.price} \xB1$${price.confidence} Status: ${PriceStatus[price.status]}`)
-}
